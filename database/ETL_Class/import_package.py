98e59dfd62e35dd2a4dfaf031d90b887aec650fe
Jeong Eunji <45502554+eunjiJeong729@users.noreply.github.com>
2020-12-07 20:40:21 +0900

    Delete import_package.py

--- database/ETL_Class/import_package.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-import re
-import geopandas as gpd
-import pandas as pd
-import psycopg2 as pg
-import numpy as np
-from sqlalchemy import create_engine
-from shapely.geometry import Point, LineString
